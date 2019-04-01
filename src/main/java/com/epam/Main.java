7bf77e25cb22c6e037a7f5462b95d220bcc3de3e
Пользователь <jshcherbakova@gmail.com>
2019-04-01 19:28:10 +0300

    Homework2 commit

---
+++ src/main/java/com/epam/Main.java
@@ -0,0 +1,11 @@
+package com.epam;
+
+import org.junit.Assert;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Shcherbakova Yulia");
+
+        Assert.fail("This is fail");
+    }
+}
